9ff44c12480115c600f92740ce3f0f3b4cc1cb09
Sriramakrishnan S <sriramakrishnan005@gmail.com>
2023-07-04 18:40:14 +0530

    Ex1B - Matrix Multiplication

---
+++ matrixMultiplication.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+    int r1, r2, c1, c2;
+    printf("Enetr no of rows of matrix 1: ");
+    scanf("%d", &r1);
+    printf("Enetr no of rows of matrix 2: ");
+    scanf("%d", &r2);
+    printf("Enetr no of Columns of matrix 1: ");
+    scanf("%d", &c1);
+    printf("Enetr no of Columns of matrix 2: ");
+    scanf("%d", &c2);
+
+    if (c2 != r1)
+    {
+        printf("Matrix Multiplication not possible!");
+        return 0;
+    }
+
+    int mat1[r1][c1], mat2[r2][c2], result[r1][c2];
+    int i, j, k;
+
+    printf("Enetr Elements for MAtrix 1: ");
+    for (i = 0; i < r1; i++)
+        for (j = 0; j < c1; j++)
+            scanf("%d", &mat1[i][j]);
+
+    printf("Enetr Elements for MAtrix 2: ");
+    for (i = 0; i < r1; i++)
+        for (j = 0; j < c1; j++)
+            scanf("%d", &mat2[i][j]);
+
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c2; j++)
+        {
+            result[i][j] = 0;
+            for (k = 0; k < r2; k++)
+            {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+
+    printf("Resultant Matrix : \n");
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c2; j++)
+        {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+}
