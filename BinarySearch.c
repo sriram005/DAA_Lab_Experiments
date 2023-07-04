5f4d91ac30498babd0a7384aa70645b864a60fe9
Sriramakrishnan S <sriramakrishnan005@gmail.com>
2023-07-04 18:28:22 +0530

    Ex1A - Binary Search

---
+++ BinarySearch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int BinarySearch(int arr[], int low, int high, int x)
+{
+    int mid = (low + high) / 2;
+    if (arr[mid] == x)
+        return mid;
+    else if (arr[mid] > x)
+        return BinarySearch(arr, low, mid - 1, x);
+    else
+        return BinarySearch(arr, mid + 1, high, x);
+}
+
+int main()
+{
+    int n, i;
+    printf("Enter no of Elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the Elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    int x;
+    printf("Enetr the elements to search : ");
+    scanf("%d", &x);
+    printf("%d is found at the position : %d", x, BinarySearch(arr, 0, n - 1, x));
+}
